cc1d588cf35f0eefcc6f1280cea9d2be674eeb80
Somesh Singh <someshsingh298@gmail.com>
2022-01-08 00:47:33 +0530

    added flags in front of channel name

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var underlineButton = document.getElementById("flag_checkbox")
-    if (underlineButton) {
-        underlineButton.addEventListener("click", () => {
-            if (underlineButton.checked == true) {
+    var showFlag = document.getElementById("flag_checkbox")
+    if (showFlag) {
+        showFlag.addEventListener("click", () => {
+            if (showFlag.checked == true) {
                 chrome.tabs.executeScript({
                     file: "scripts/show_flag.js"
                 })
+                showsFlag()
             } else {
                 chrome.tabs.executeScript({
                     file: "scripts/remove_flag.js"
--- scripts/show_flag.js
+++ scripts/show_flag.js
@@ -1,5 +1,34 @@
-var links = document.getElementsByTagName("a")
 
+var apiKey="AIzaSyB0rBKSzFziVKcMkjqcsrNdRLEjohwLnRM"
+
+// selecting video from the page
+var links = document.querySelectorAll('div#meta')
 for (let i = 0; i < links.length; i++) {
-    links[i].style.textDecoration = "underline"
+    var videoTitle = links[i].querySelector('h3')
+    var videoLink = videoTitle.querySelector('a')
+    var x = String(videoLink)
+    var videoId = x.slice(32,43)
+
+    // getting channel id from video
+    const getChannelId = async () => {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`)
+        const data = await response.json();
+        getChannelCountry(data.items[0].snippet.channelId)
+    }
+    getChannelId()
+
+    // getting country from channel id
+    const getChannelCountry = async (channelId) => {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${apiKey}`)
+        const data = await response.json();
+        displayChannelsCountryFlag(data.items[0].snippet.country)
+    }
+
+    const displayChannelsCountryFlag = async (countryCode) => {
+        var channelTitle = links[i].querySelector('#byline-container')
+        console.log(channelTitle)
+        var img = new Image(23,12) 
+        img.src = `https://countryflagsapi.com/png/${countryCode}`
+        channelTitle.appendChild(img);
+    }
 }
